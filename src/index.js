e5871f40fbd59311c51bbcf776e9efa628588474
Leonardo Ramos <leo.ramos.ari@gmail.com>
2020-07-13 00:47:45 -0500

    what is jsx

--- src/index.js
+++ src/index.js
@@ -9,7 +9,35 @@ container.appendChild(element); */
 import React from "react";
 import ReactDOM from "react-dom";
 
-const element = <h1>hello, project react</h1>;
-const container = document.getElementById("app");
+/* const element = <h1>hello, project react</h1>; */
+/* const jsx = <h1>hello, project react</h1>; */
+/* const element = React.createElement(
+  "a",
+  { href: "https://platzi.com" },
+  "ir a platzi"
+); */
+/* const name = "lesalo"; */
+/* const element = React.createElement("h1", {}, `hola, soy ${name}`); */
+/* const sum = () => 3 + 3; */
+/* const jsx = <h1>hola soy, {__aqui__va__expresion__}</h1>; */
+/* const jsx = <h1>hola soy, {2 + 2}</h1>; */
+/* const jsx = <h1>hola soy, {sum()}</h1>; */
 
+const jsx = (
+  <div>
+    <h1>Hola soy leo</h1>
+    <p>soy ingeniero fronted</p>
+  </div>
+);
+
+const element = React.createElement(
+  "div",
+  {},
+  React.createElement("h1", {}, "Hola soy leo"),
+  React.createElement("p", {}, "soy ingeniero de la web")
+);
+
+const container = document.getElementById("app");
+/* 
+ReactDOM.render(__qué__,__dónde__); */
 ReactDOM.render(element, container);
