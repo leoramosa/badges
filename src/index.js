82ffdb3d11ed5b21ffb85283ecead46d6f741cf8
Leonardo Ramos <leo.ramos.ari@gmail.com>
2020-07-13 00:25:29 -0500

    change import with react

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-const element = document.createElement("h1");
+/* tradition code of javascript */
+/* const element = document.createElement("h1");
 element.innerText = "Hello Project";
 
 const container = document.getElementById("app");
 
-container.appendChild(element);
+container.appendChild(element); */
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const element = <h1>hello, project react</h1>;
+const container = document.getElementById("app");
+
+ReactDOM.render(element, container);
