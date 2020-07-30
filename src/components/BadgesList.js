d4d8aef12aba2c14c0a9e510098e7d98fbeabf10
Leonardo Ramos <leo.ramos.ari@gmail.com>
2020-07-30 18:47:35 -0500

    se creo el api y funciona el get

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,8 +1,25 @@
 import React from "react";
 import "./styles/BadgesList.css";
+import { Link } from "react-router-dom";
+import Skeleton from "@material-ui/lab/Skeleton";
+/* this.props.badges.payload.map((badge) if is of url www...payload <-- add  */
 
 class BadgesList extends React.Component {
+  state = {
+    loading: true,
+    badges: undefined,
+  };
   render() {
+    if (this.props.badges.length === 0) {
+      return (
+        <div>
+          <h3>No badges were found</h3>
+          <Link className="btn btn-primary" to="/badges/new">
+            Create new badge
+          </Link>
+        </div>
+      );
+    }
     return (
       <div className="Badgeslist__container">
         <ul className="list-unstyled">
@@ -10,11 +27,20 @@ class BadgesList extends React.Component {
             return (
               <li className="BadgesList__item" key={badge.id}>
                 <div className="BadgesList__img">
-                  <img
-                    className="BadgesList__img-container"
-                    src={badge.avatarUrl}
-                    alt=""
-                  />
+                  {!this.state.loading || this.badges ? (
+                    <Skeleton
+                      animation="wave"
+                      variant="circle"
+                      width={40}
+                      height={40}
+                    />
+                  ) : (
+                    <img
+                      className="BadgesList__img-container"
+                      src={badge.avatarUrl}
+                      alt=""
+                    />
+                  )}
                 </div>
                 <div className="">
                   <p className="Badges_Names">
