f3d93f79f6f3b3074a5865e27005d8dde9edc07d
Leonardo Ramos <leo.ramos.ari@gmail.com>
2020-08-05 22:23:28 -0500

    finally

---
+++ build/precache-manifest.12fa5a0552cbf47f7f668e65ff4fbd9f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d5e595c6b486c5e63e3",
+    "url": "/static/css/main.ffbcf576.chunk.css"
+  },
+  {
+    "revision": "6d5e595c6b486c5e63e3",
+    "url": "/static/js/main.6d5e595c.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "8ac961253825dd0bcc89",
+    "url": "/static/css/2.307f18c4.chunk.css"
+  },
+  {
+    "revision": "8ac961253825dd0bcc89",
+    "url": "/static/js/2.8ac96125.chunk.js"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "44e6cf7b16e225bf93ceb3689fd7d987",
+    "url": "/index.html"
+  }
+];
